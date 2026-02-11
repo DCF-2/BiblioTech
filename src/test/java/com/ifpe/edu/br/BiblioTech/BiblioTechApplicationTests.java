b34cfe4fe7afde33078ed817037951642e9f4084
Davi Freitas <davicfreitas1@gmail.com>
2026-02-11 09:39:00 -0300

    Initial commit

---
+++ src/test/java/com/ifpe/edu/br/BiblioTech/BiblioTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifpe.edu.br.BiblioTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
